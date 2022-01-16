565da59e8911c5ddc89071e85eff797e5d0c7115
git <bookandmusic@yeah.net>
2022-01-16 09:56:18 +0800

    version 1.01

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^!ltx0q5q!(!)w%y1e!5lsyyvf&+=$e4-!00^7j9wu71#9w@hu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
