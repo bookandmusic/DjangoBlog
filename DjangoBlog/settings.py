df087b30afff555a43ebef3da7515b443c15a580
liushaofeng <bookandmusic@yeah.net>
2023-08-28 08:07:29 +0800

    feat(*): 修改模板静态资源

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -131,14 +131,13 @@ MEDIA_URL = '/media/'  # 你上传的文件和图片会默认存在/uploads/edit
 STATIC_URL = '/static/'
 # 当运行 python manage.py collectstatic 的时候
 # STATIC_ROOT 文件夹 是用来将所有STATICFILES_DIRS中所有文件夹中的文件，以及各app中static中的文件都复制过来
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATIC_ROOT = ''
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # 其它 存放静态文件的文件夹，可以用来存放项目中公用的静态文件，里面不能包含 STATIC_ROOT
 # 如果不想用 STATICFILES_DIRS 可以不用，都放在 app 里的 static 中也可以
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 CONSTANT = {
     'page_size': 10
